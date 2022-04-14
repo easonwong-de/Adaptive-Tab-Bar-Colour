66fabee2a230a8cd9c4f41d6686cc5b538679cff
Eason Wong <wangyatsing@gmail.com>
2022-04-14 04:40:05 +0200

    v1.0

--- content_script.js
+++ content_script.js
@@ -52,6 +52,11 @@ function hexBrightness(hex) {
 //from TimDown@stackoverflow.com
 //Convert hex color (String) to rgb (Object)
 function hexToRgb(hex) {
+	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
 	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 	return result ? {
 		r: parseInt(result[1], 16),
