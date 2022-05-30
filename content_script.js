be318d8a21b499eb5b87126cdd202bd474e5c915
Eason Wong <wangyatsing@gmail.com>
2022-05-30 22:27:37 +0200

    Update v1.5.4

--- content_script.js
+++ content_script.js
@@ -8,7 +8,7 @@ reserved color is a color => it is the theme color
 reserved color is a IGNORE => use calculated color as theme color
 reserved color is a tag name => theme color is stored under that tag
 reserved color is a class name => theme color is stored under that class */
-const reservedColor_cs = {
+const reservedColor = {
 	"open.spotify.com": "rgb(0, 0, 0)",
 	"mail.google.com": "CLASS: wl",
 	"www.youtube.com": "TAG: ytd-masthead",
@@ -18,7 +18,7 @@ const reservedColor_cs = {
 	"developer.mozilla.org": "IGNORE_THEME"
 };
 
-var port;
+var Port;
 
 findColor();
 
@@ -26,9 +26,9 @@ findColor();
  * Finds color and send to background.
  */
 function findColor() {
-	port = browser.runtime.connect();
+	Port = browser.runtime.connect();
 	if (!findColorReserved()) findColorUnreserved();
-	if (!document.hidden) port.postMessage({ color: response_color });
+	if (!document.hidden) Port.postMessage({ color: response_color });
 }
 
 //Updates color when user makes action
@@ -39,8 +39,8 @@ function findColor() {
 //document.onscroll = findColor;
 
 //Updates color when Dark Reader changes mode
-var onDarkReader = new MutationObserver(findColor);
-onDarkReader.observe(document.documentElement, { attributes: true, attributeFilter: ["data-darkreader-mode"] });
+var ondarkreader = new MutationObserver(findColor);
+ondarkreader.observe(document.documentElement, { attributes: true, attributeFilter: ["data-darkreader-mode"] });
 
 //Remind background.js of the color
 browser.runtime.onMessage.addListener(
@@ -59,22 +59,22 @@ browser.runtime.onMessage.addListener(
  */
 function findColorReserved() {
 	let host = document.location.host; // e.g. "host" can be "www.irgendwas.com"
-	if (reservedColor_cs[host] == null) {
+	if (reservedColor[host] == null) {
 		return false;
-	} else if (reservedColor_cs[host] == "IGNORE_THEME") {
+	} else if (reservedColor[host] == "IGNORE_THEME") {
 		response_color = getComputedColor();
-	} else if (reservedColor_cs[host].startsWith("TAG: ")) {
-		let tagName = reservedColor_cs[host].replace("TAG: ", "");
+	} else if (reservedColor[host].startsWith("TAG: ")) {
+		let tagName = reservedColor[host].replace("TAG: ", "");
 		let el_list = document.getElementsByTagName(tagName);
 		if (el_list.length == 0) return false;
 		response_color = getColorFrom(el_list[0]);
-	} else if (reservedColor_cs[host].startsWith("CLASS: ")) {
-		let className = reservedColor_cs[host].replace("CLASS: ", "");
+	} else if (reservedColor[host].startsWith("CLASS: ")) {
+		let className = reservedColor[host].replace("CLASS: ", "");
 		let el_list = document.getElementsByClassName(className);
 		if (el_list.length == 0) return false;
 		response_color = getColorFrom(el_list[0]);
 	} else {
-		response_color = reservedColor_cs[host];
+		response_color = reservedColor[host];
 	}
 	if (response_color == "") {
 		return false;
@@ -97,7 +97,7 @@ function findColorUnreserved() {
 /** 
  * @returns Provided theme-color e.g. "#ffffff", "rgba(33, 33, 33, 0.98)"
  */
-function getThemeColor() {
+ function getThemeColor() {
 	//Get theme-color defined by the website html
 	headerTag = document.querySelector('meta[name="theme-color"]');
 	if (headerTag == null) {
@@ -118,17 +118,14 @@ function getComputedColor() {
 	for (let element = document.elementFromPoint(window.innerWidth / 2, 3); element; element = element.parentElement) {
 		color = overlayColor(color, anyToRgba(getColorFrom(element)));
 	}
-	if (color.a != 1) {
-		let body = document.getElementsByTagName("body");
-		if (body.length == 0) {
-			color = "";
-		} else {
-			color = getColorFrom(body[0]);
-			if (color.includes("rgba")) color = "";
-		}
+	if (color.a == 0) {
+		let body = document.getElementsByTagName("body")[0];
+		if (body == undefined) return "";
+		color = getColorFrom(body);
+		if (color.includes("rgba")) color = "";
 		return color;
 	} else {
-		return "rgb(" + color.r + ", " + color.g + ", " + color.b + ")";
+		return "rgb(" + color.r + ", " + color.g + ", " + color.b + ", " + color.a + ")";
 	}
 }
 
@@ -138,5 +135,85 @@ function getComputedColor() {
  */
 function getColorFrom(element) {
 	let color = getComputedStyle(element).backgroundColor;
-	return color ? color : "rgba(0, 0, 0, 0)";
+	if (color == null) color = "rgba(0, 0, 0, 0)";
+	return color;
+}
+
+/**
+ * @param {string} color Color in string
+ * @returns Color in object
+ */
+function anyToRgba(color) {
+	return color.startsWith("#") ? hexToRgba(color) : rgbaToRgba(color);
+}
+
+/**
+ * Converts hex color (String) to rgb (Object).
+ * @author TimDown stackoverflow.com
+ * 
+ * @param {string} hex color in hex
+ * @returns color in object
+ */
+function hexToRgba(hex) {
+	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16),
+		a: 1
+	} : null;
+}
+
+/**
+ * Converts rgba/rgb (String) to rgba (Object).
+ * 
+ * @param {string} rgbaString color in rgba/rgb
+ * @returns color in object
+ */
+function rgbaToRgba(rgbaString) {
+	var result = rgbaString.match(/[.?\d]+/g).map(Number);
+	if (result.length == 3) result[3] = 1;
+	return result ? {
+		r: result[0],
+		g: result[1],
+		b: result[2],
+		a: result[3]
+	} : null;
+}
+
+/**
+ * Add up colors
+ * 
+ * @param {object} top Color on top
+ * @param {object} bottom Color underneath
+ * @returns Result of the addition in object
+ */
+function overlayColor(top, bottom) {
+	let a = (1 - top.a) * bottom.a + top.a;
+	if (a == 0) {
+		return { r: 0, g: 0, b: 0, a: 0 };
+	} else {
+		return {
+			r: ((1 - top.a) * bottom.a * bottom.r + top.a * top.r) / a,
+			g: ((1 - top.a) * bottom.a * bottom.g + top.a * top.g) / a,
+			b: ((1 - top.a) * bottom.a * bottom.b + top.a * top.b) / a,
+			a: a
+		}
+	}
+}
+
+/**
+ * Deletes alpha value from rgba (String).
+ * 
+ * @param {string} color color in rgba e.g. "rgba(33, 33, 33, 0.98)"
+ * @returns color in rgb
+ */
+function noAlphaValue(color) {
+	color = color.replace("rgba", "rgb");
+	color = color.slice(0, color.lastIndexOf(","));
+	color = color + ")";
+	return color;
 }
