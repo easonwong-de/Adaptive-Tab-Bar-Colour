6c4e9bb2564697a36ec69b5dd1b26183060fa7c6
Eason Wong <wangyatsing@gmail.com>
2022-06-09 02:46:11 +0200

    Update v1.5.7

--- content_script.js
+++ content_script.js
@@ -163,9 +163,9 @@ function anyToRgba(color) {
  */
 function hexToRgba(hex) {
 	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
 	hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 	return result ? {
 		r: parseInt(result[1], 16),
 		g: parseInt(result[2], 16),
@@ -177,11 +177,11 @@ function hexToRgba(hex) {
 /**
  * Converts rgba/rgb (String) to rgba (Object).
  * 
- * @param {string} rgbaString color in rgba/rgb
+ * @param {string} rgba color in rgba/rgb
  * @returns color in object
  */
-function rgbaToRgba(rgbaString) {
-	var result = rgbaString.match(/[.?\d]+/g).map(Number);
+function rgbaToRgba(rgba) {
+	let result = rgba.match(/[.?\d]+/g).map(Number);
 	if (result.length == 3) result[3] = 1;
 	return result ? {
 		r: result[0],
@@ -190,6 +190,12 @@ function rgbaToRgba(rgbaString) {
 		a: result[3]
 	} : null;
 }
+
+/*function hslToRgba(hsl) {
+	let result = hsl.match(/[.?\d]+/g).map(Number);
+
+}
+*/
 
 /**
  * Add up colors
