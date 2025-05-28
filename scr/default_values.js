5c4efa1fcbb8034eec2c5493594daed140e6de1e
Eason Wong <me@easonwong.de>
2025-05-28 20:45:28 +0200

    update default colours

--- scr/default_values.js
+++ scr/default_values.js
@@ -17,20 +17,20 @@ export const default_fallbackColour_dark = "#2b2a33";
  */
 export const aboutPageColour = Object.freeze({
 	checkerboard: { light: "DEFAULT", dark: undefined },
-	deleteprofile: { light: "DEFAULT", dark: "rgb(43, 42, 51)" },
-	"devtools-toolbox": { light: "DEFAULT", dark: "rgb(12, 12, 13)" },
-	editprofile: { light: "DEFAULT", dark: "rgb(43, 42, 51)" },
+	deleteprofile: { light: "DEFAULT", dark: "#2b2a33" },
+	"devtools-toolbox": { light: "DEFAULT", dark: "#0c0c0d" },
+	editprofile: { light: "DEFAULT", dark: "#2b2a33" },
 	firefoxview: { light: "HOME", dark: "HOME" },
 	home: { light: "HOME", dark: "HOME" },
 	logo: { light: undefined, dark: "IMAGEVIEWER" },
-	mozilla: { light: undefined, dark: "rgb(128, 0, 0)" },
+	mozilla: { light: undefined, dark: "#800000" },
 	newtab: { light: "HOME", dark: "HOME" },
-	newprofile: { light: "DEFAULT", dark: "rgb(43, 42, 51)" },
-	performance: { light: "DEFAULT", dark: "rgb(35, 34, 42)" },
-	plugins: { light: "DEFAULT", dark: "rgb(43, 42, 51)" },
-	privatebrowsing: { light: undefined, dark: "rgb(37, 0, 62)" },
-	processes: { light: "rgb(238, 238, 238)", dark: "rgb(50, 49, 58)" },
-	"sync-log": { light: "rgb(236, 236, 236)", dark: "rgb(40, 40, 40)" },
+	newprofile: { light: "DEFAULT", dark: "#2b2a33" },
+	performance: { light: "DEFAULT", dark: "#23222a" },
+	plugins: { light: "DEFAULT", dark: "#2b2a33" },
+	privatebrowsing: { light: undefined, dark: "#25003e" },
+	processes: { light: "#eeeeee", dark: "#32313a" },
+	"sync-log": { light: "#ececec", dark: "#282828" },
 });
 
 /**
@@ -38,13 +38,13 @@ export const aboutPageColour = Object.freeze({
  */
 export const restrictedSiteColour = Object.freeze({
 	"accounts-static.cdn.mozilla.net": { light: "DEFAULT", dark: "DEFAULT" },
-	"accounts.firefox.com": { light: "rgb(250, 250, 253)", dark: undefined },
+	"accounts.firefox.com": { light: "#fafafd", dark: undefined },
 	"addons.cdn.mozilla.net": { light: "DEFAULT", dark: "DEFAULT" },
-	"addons.mozilla.org": { light: undefined, dark: "rgb(32, 18, 58)" },
+	"addons.mozilla.org": { light: undefined, dark: "#20123a" },
 	"content.cdn.mozilla.net": { light: "DEFAULT", dark: "DEFAULT" },
-	"discovery.addons.mozilla.org": { light: "rgb(236, 236, 236)", dark: undefined },
+	"discovery.addons.mozilla.org": { light: "#ececec", dark: undefined },
 	"install.mozilla.org": { light: "DEFAULT", dark: "DEFAULT" },
-	"support.mozilla.org": { light: "rgb(255, 255, 255)", dark: undefined },
+	"support.mozilla.org": { light: "#ffffff", dark: undefined },
 });
 
 /**
@@ -55,13 +55,15 @@ export const restrictedSiteColour = Object.freeze({
  * @todo Adds light / dark attributes.
  */
 export const recommendedAddonPageColour = Object.freeze({
-	"adguardadblocker@adguard.com": "#131313",
-	"enhancerforyoutube@maximerf.addons.mozilla.org": "#282a2d",
-	"languagetool-webextension@languagetool.org": "#111113",
-	"sponsorBlocker@ajay.app": "#323232",
-	"tongwen@softcup": "#fffffe",
-	"uBlock0@raymondhill.net": "#1b1a23",
-	"{46551EC9-40F0-4e47-8E18-8E5CF550CFB8}": "#fffffe",
-	"{ce9f4b1f-24b8-4e9a-9051-b9e472b1b2f2}": "#fffffe",
-	"{e7476172-097c-4b77-b56e-f56a894adca9}": "#151f2a",
+	"addon@darkreader.org": "#141e24", // Dark Reader
+	"adguardadblocker@adguard.com": "#1f1f1f", // AdGuard AdBlocker
+	"deArrow@ajay.app": "#333333", // DeArrow
+	"enhancerforyoutube@maximerf.addons.mozilla.org": "#292a2d", // Enhancer for YouTube™
+	"languagetool-webextension@languagetool.org": "#111213", // LanguageTool
+	"sponsorBlocker@ajay.app": "#333333", // SponsorBlock for YouTube
+	"uBlock0@raymondhill.net": "#1b1b24", // uBlock Origin
+	"{036a55b4-5e72-4d05-a06c-cba2dfcc134a}": "#171a1b", // Translate Web Pages
+	"{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}": "#242424", // Stylus
+	"{aecec67f-0d10-4fa7-b7c7-609a2db280cf}": "#262626", // Violentmonkey
+	"{ce9f4b1f-24b8-4e9a-9051-b9e472b1b2f2}": "#1c1b1f", // Clear Browsing Data
 });
