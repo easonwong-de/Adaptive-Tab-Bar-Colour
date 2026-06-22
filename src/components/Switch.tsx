bae0e3f520d55d1f56c025019ff455c9079881e7
Eason Wong <me@easonwong.de>
2026-06-22 23:02:46 +0200

    fix: fix switch card alignment in advanced settings tab

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -2,23 +2,18 @@ import clsx from "clsx";
 import styles from "./Switch.module.css";
 
 interface SwitchProps {
-	label?: string;
 	active?: boolean;
 	onChange: (nextValue: boolean) => void;
 }
 
 export default function Switch({
-	label = "",
 	active = false,
 	onChange,
 }: SwitchProps) {
 	return (
-		<label className={styles.switch}>
-			<button
-				className={clsx(active && styles.selected)}
-				onClick={() => onChange(!active)}
-			></button>
-			<p>{label}</p>
-		</label>
+		<button
+			className={clsx(styles.switch, active && styles.active)}
+			onClick={() => onChange(!active)}
+		></button>
 	);
 }
